f7f94cb51aa3eb9eae751cd9cb6d303e9e2bf87b
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2022-01-05 12:33:30 -0500

    String theory again

--- stringz.py
+++ stringz.py
@@ -1,55 +1,55 @@
-# first = "This is my first string"
-# second = 'This is my second string'
-# third = 'This is my "third" string'
-# # delimiters
-#
-# # bad = "This has a "double quote" in it"
-# better = "This one also has a \"double quote\" in it\n"
-# best = 'This is the best way to "double quote" something in python'
-# print(best)
-# best_upper = best.upper()
-# print(best_upper)
-# print(best)
-#
-# paragraph = first + second + third
-# print(paragraph)
-#
-# pretty_para = first + ".  " + second + ".  " + third + "."
-# print(pretty_para)
-#
-# name = "fluffy"
-# age = 7
-# color = "white"
-#
-# # concatenation
-# mycat = "My cat is named " + name + " and is the color " + color + " and is " + str(age) + " years old."
-# print(mycat)
-#
-#
-# # f-string
-# yourcat = f"Your cat is named {name} and is the color {color} and is {age} years old."
-# print(yourcat)
-#
-# bdaycat = f"Your cat is named {name} and is the color {color} and just turned {age + 1} years old."
-# print(bdaycat)
-#
-#
-# # format method
-# hiscat = "His cat is named {} and is the color {} and is {} years old.".format(name, color, age)
-# print(hiscat)
-#
-# cats = ["fluffy", "snowball", "garfield"]
-# kitty = "My cats are: {}, {}, and {}".format(*cats)
-# print(kitty)
-#
-# diff_order = "My cats are: {2}, {2}, and {0}".format("fluffy", "snowball", "garfield")
-# print(diff_order)
-# # doc string
-# """
-# This 'is'
-# a "multiline"
-# comment.
-# """
+first = "This is my first string"
+second = 'This is my second string'
+third = 'This is my "third" string'
+# delimiters
+
+# bad = "This has a "double quote" in it"
+better = "This one also has a \"double quote\" in it\n"
+best = 'This is the best way to "double quote" something in python'
+print(best)
+best_upper = best.upper()
+print(best_upper)
+print(best)
+
+paragraph = first + second + third
+print(paragraph)
+
+pretty_para = first + ".  " + second + ".  " + third + "."
+print(pretty_para)
+
+name = "fluffy"
+age = 7
+color = "white"
+
+# concatenation
+mycat = "My cat is named " + name + " and is the color " + color + " and is " + str(age) + " years old."
+print(mycat)
+
+
+# f-string
+yourcat = f"Your cat is named {name} and is the color {color} and is {age} years old."
+print(yourcat)
+
+bdaycat = f"Your cat is named {name} and is the color {color} and just turned {age + 1} years old."
+print(bdaycat)
+
+
+# format method
+hiscat = "His cat is named {} and is the color {} and is {} years old.".format(name, color, age)
+print(hiscat)
+
+cats = ["fluffy", "snowball", "garfield"]
+kitty = "My cats are: {}, {}, and {}".format(*cats)
+print(kitty)
+
+diff_order = "My cats are: {2}, {2}, and {0}".format("fluffy", "snowball", "garfield")
+print(diff_order)
+# doc string
+"""
+This 'is'
+a "multiline"
+comment.
+"""
 
 movie = "Usual Suspects"
 print(movie)
